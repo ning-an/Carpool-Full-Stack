e8460c9175afb87c04b779931503c3df20f1d347
Ning An <ning.an@outlook.com>
2020-09-11 19:31:12 -0400

    Update package.json Files

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+require("dotenv").config();
+const passport = require("passport");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Passport config
+require("./server/config/passport")(passport);
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Session Middelware
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+
+// User register and login
+app.use("/api/users", require("./server/routes/users"));
+
+// Trips
+app.use("/api/trips", require("./server/routes/trips"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Listen
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
---
+++ src/components/auth/Register.js
@@ -0,0 +1,284 @@
+import React, { useState, useEffect } from "react";
+import { NavLink, Link } from "react-router-dom";
+import styled from "styled-components";
+import { useParams, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { COLORS } from "../../Constants";
+import { validateData, signUp } from "../../helpers/api-helper";
+import { RegisterSuccess, LoginSuccess } from "../../reducer/actions";
+
+export default function Register() {
+  const { role } = useParams();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cfpassword, setCfpassword] = useState("");
+  const [make, setMake] = useState("");
+  const [model, setModel] = useState("");
+  const [plate, setPlate] = useState("");
+  const [seats, setSeats] = useState(0);
+  const [error, setError] = useState(null);
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  // Authentication check
+  const checkAuth = async () => {
+    const res = await fetch("/api/users/login");
+    if (!res.ok) {
+      const user = await res.json();
+      dispatch(LoginSuccess(user));
+      history.push("/");
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const errors = validateData({
+      name,
+      email,
+      password,
+      cfpassword,
+      make,
+      model,
+      plate,
+      seats,
+      driver: role === "driver",
+    });
+    if (errors.length > 0) {
+      setError(errors);
+    } else {
+      const res = await signUp();
+      if (!res.ok) {
+        const jsonData = await res.json();
+        setError([...errors, jsonData]);
+      } else {
+        const jsonDataSuccess = await res.json();
+        dispatch(RegisterSuccess(jsonDataSuccess.msg));
+        history.push("/login");
+      }
+    }
+  };
+  return (
+    <Wrapper>
+      <h1>Sign up</h1>
+      <Tabs>
+        <li>
+          <NavLink to="/register/passenger" activeClassName="selected">
+            Passenger
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/register/driver" activeClassName="selected">
+            Driver
+          </NavLink>
+        </li>
+      </Tabs>
+      <Form onSubmit={handleSubmit}>
+        {error && (
+          <ErrorMsg>
+            {error.map((err, index) => (
+              <li key={index}>{err.msg}</li>
+            ))}
+          </ErrorMsg>
+        )}
+        <Line />
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <label htmlFor="cfpassword">Confirm Password</label>
+        <input
+          type="password"
+          name="cfpassword"
+          id="cfpassword"
+          value={cfpassword}
+          onChange={(e) => setCfpassword(e.target.value)}
+          required
+        />
+        {role === "driver" && (
+          <>
+            <h2>Car Information</h2>
+            <label htmlFor="make">Make</label>
+            <input
+              type="text"
+              name="make"
+              id="make"
+              value={make}
+              onChange={(e) => setMake(e.target.value)}
+              required
+            />
+            <label htmlFor="model">Model</label>
+            <input
+              type="text"
+              name="model"
+              id="model"
+              value={model}
+              onChange={(e) => setModel(e.target.value)}
+              required
+            />
+            <label htmlFor="plate">Plate No.</label>
+            <input
+              type="text"
+              name="plate"
+              id="plate"
+              value={plate}
+              onChange={(e) => setPlate(e.target.value)}
+              required
+            />
+            <label htmlFor="seats">Available Seats</label>
+            <input
+              type="number"
+              name="seats"
+              id="seats"
+              min="1"
+              max="5"
+              value={seats}
+              onChange={(e) => setSeats(e.target.value)}
+              required
+            />
+          </>
+        )}
+        <button type="submit">Register</button>
+        <p>
+          Already have an account? <Link to="/login"> Login</Link>
+        </p>
+      </Form>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    margin: 30px;
+    font-size: 32px;
+  }
+`;
+
+const Tabs = styled.ul`
+  display: flex;
+  position: relative;
+  left: -100px;
+  top: 5px;
+
+  li {
+    background-color: ${COLORS.apricot};
+    padding: 10px 20px 10px;
+    margin-right: 10px;
+    transform: skewX(-20deg);
+    border-radius: 4px;
+
+    a {
+      display: block;
+      transform: skewX(20deg);
+      filter: brightness(80%);
+
+      &:visited {
+        color: white;
+      }
+    }
+    .selected {
+      font-weight: bold;
+      filter: brightness(100%);
+    }
+  }
+`;
+
+const ErrorMsg = styled.ul`
+  li {
+    list-style: square;
+    padding: 10px;
+    margin: 10px 0;
+    background-color: ${COLORS.appleCore};
+    color: white;
+    border-radius: 4px;
+    box-shadow: 0 0 5px 1px lightgrey;
+  }
+`;
+
+const Form = styled.form`
+  width: 400px;
+  padding: 20px;
+  border-radius: 4px;
+  box-shadow: 0 0 5px 2px lightgrey;
+  z-index: 2;
+  background-color: white;
+
+  label {
+    display: block;
+    margin-top: 10px;
+    margin-bottom: 5px;
+  }
+
+  input {
+    box-sizing: border-box;
+    width: 100%;
+    height: 36px;
+    border-radius: 4px;
+    border: none;
+    box-shadow: 0 0 3px 1px lightgrey;
+    padding-left: 16px;
+  }
+
+  h2 {
+    margin-top: 30px;
+    border-top: 1px solid ${COLORS.apricot};
+    padding-top: 10px;
+  }
+
+  button {
+    box-sizing: border-box;
+    width: 100%;
+    height: 36px;
+    font-size: 100%;
+    background-color: ${COLORS.apricot};
+    color: white;
+    border-radius: 4px;
+    border: none;
+    margin: 20px 0;
+  }
+
+  a {
+    color: ${COLORS.citrus};
+
+    &:visited {
+      color: ${COLORS.apricot};
+    }
+  }
+`;
+
+const Line = styled.div`
+  border-top: 1px solid ${COLORS.apricot};
+`;
